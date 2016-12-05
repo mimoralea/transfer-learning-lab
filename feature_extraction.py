e67e86b2a31df790eb16b6232a250d6d290c70ac
Miguel Morales <mimoralea@gmail.com>
2016-12-05 15:47:19 -0600

    Feature extraction lab - VGG, ResNet and Inception around 85% accuracy with these

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,13 +1,20 @@
 import pickle
 import tensorflow as tf
-# TODO: import Keras layers you need here
+
+import numpy as np
+from keras.layers import Input, Flatten, Dense
+from keras.models import Model
+
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 # command line flags
-flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
-flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
+flags.DEFINE_string('training_file', '', 'Bottleneck features training file (.p)')
+flags.DEFINE_string('validation_file', '', 'Bottleneck features validation file (.p)')
+
+flags.DEFINE_integer('epochs', 50, 'The number of epochs.')
+flags.DEFINE_integer('batch_size', 256, 'The batch size.')
 
 
 def load_bottleneck_data(training_file, validation_file):
@@ -36,19 +43,27 @@ def load_bottleneck_data(training_file, validation_file):
 
 def main(_):
     # load bottleneck data
-    X_train, y_train, X_val, y_val = load_bottleneck_data(FLAGS.training_file, FLAGS.validation_file)
+    X_train, y_train, X_val, y_val = load_bottleneck_data(
+        FLAGS.training_file, FLAGS.validation_file)
 
-    print(X_train.shape, y_train.shape)
-    print(X_val.shape, y_val.shape)
-
-    # TODO: define your model and hyperparams here
-    # make sure to adjust the number of classes based on
-    # the dataset
-    # 10 for cifar10
-    # 43 for traffic
-
-    # TODO: train your model here
-
+    nb_classes = len(np.unique(y_train))
+    
+    # define your model and hyperparams here
+    input_shape = X_train.shape[1:]
+    inp = Input(shape=input_shape)
+    x = Flatten()(inp)
+    x = Dense(nb_classes, activation='softmax')(x)
+    model = Model(inp, x)
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    
+    # train your model here
+    model.fit(X_train, y_train,
+              nb_epoch=FLAGS.epochs,
+              batch_size=FLAGS.batch_size,
+              validation_data=(X_val, y_val),
+              shuffle=True)
 
 # parses flags and calls the `main` function above
 if __name__ == '__main__':
